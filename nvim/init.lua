fea20d42daaa79ceaa9ee08db919914d5965b4da
guge <gugeemail@qq.com>
2024-11-08 20:01:32 +0800

    fix config bug -- provider.python

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,5 @@ require("config.options") -- 选项
 require("config.keymaps") -- 按键设置
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- win特别注意python解释器路径，环境变量，pip安装要注意upgrade
+vim.g.python3_host_prog = 'python.exe'
